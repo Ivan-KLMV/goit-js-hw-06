3829a7e108b602300eb6b2382f0505fa2ec83ef3
Ivan Klimov <klimov128@gmail.com>
2023-02-11 22:40:40 +0200

    Task#2

--- js/task-01.js
+++ js/task-01.js
@@ -7,3 +7,14 @@ const listCategories = [...itemCategories].map((item) => {
   console.log('Category: ', item.querySelector('h2').textContent);
   console.log('Elements: ', item.querySelectorAll('li').length);
 });
+
+// Другий варіант рішення
+
+// const listCategories = document.querySelector('#categories');
+// const categorieItem = listCategories.querySelectorAll('.item');
+// console.log(`Number of categories: ${listCategories.children.length}`);
+
+// categorieItem.forEach((el) => {
+//   console.log(`Category: ${el.querySelector('h2').textContent}`);
+//   console.log(`Elements: ${el.querySelectorAll('li').length}`);
+// });
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const createElement = (content) => {
+  return content.map((option) => {
+    const ingredientEl = document.createElement('li');
+
+    ingredientEl.textContent = option;
+    ingredientEl.classList.add('item');
+
+    return ingredientEl;
+  });
+};
+
+const listIngredients = createElement(ingredients);
+document.querySelector('#ingredients').append(...listIngredients);
