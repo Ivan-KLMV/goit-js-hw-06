73079bb7206c99fe063e952e144b721940b19395
Ivan Klimov <klimov128@gmail.com>
2023-02-15 15:58:38 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonToChangeColor = document.querySelector('button.change-color');
+buttonToChangeColor.addEventListener('click', onBackgroundColorChange);
+const currentColorHex = document.querySelector('span.color');
+
+function onBackgroundColorChange() {
+  currentColorHex.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+}
