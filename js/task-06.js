26f10c899bbe9435f0c958b474d0cbcd896daeef
Ivan Klimov <klimov128@gmail.com>
2023-02-16 19:26:07 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -4,7 +4,7 @@ const dataLength = inputValidationtEl.dataset.length;
 inputValidationtEl.addEventListener('blur', validator);
 
 function validator(event) {
-  event.currentTarget.value.length >= Number(dataLength)
+  event.currentTarget.value.length === Number(dataLength)
     ? inputValidationtEl.setAttribute('class', 'valid')
     : inputValidationtEl.setAttribute('class', 'invalid');
 }
